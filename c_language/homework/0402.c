9c590b17101dc74b94ded8c9859898d40a3d875c
scj040921 <2465618779@qq.com>
2026-07-21 19:44:54 +0800

    Update 0402.c

--- c_language/homework/0402.c
+++ c_language/homework/0402.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-    int64_t a[4] = {1, 2, 3, 4};
-    int64_t *p1=(int64_t *)(&a+1);
+    int64_t a[4] = {1, 2, 3, 4};//int64_t八个字节
+    int64_t *p1=(int64_t *)(&a+1);//&a指向整个数组的地址
     int64_t *p2=(int64_t *)((int64_t)a+1);
- 
-    printf("%llx\n", p1[-1]);
+ //a[i]==*(a+i)
+    printf("%llx\n", p1[-1]);//4
     printf("%llx\n", *p2);
     
     return 0;
